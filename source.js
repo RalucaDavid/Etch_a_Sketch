5df825889397c14b833f0b7c854b003bf88b7997
Raluca <andreearalucadavid@gmail.com>
2024-02-21 21:20:47 +0200

    Add draw mode with random colors.

--- source.js
+++ source.js
@@ -1,3 +1,5 @@
+let modeDraw = false;
+
 const gridContainer = document.querySelector('.grid-container');
 for (let i=0;i<16*16;i++)
 {
@@ -6,13 +8,26 @@ for (let i=0;i<16*16;i++)
     gridContainer.appendChild(gridItem);
 }
 
+function getRandomColor()
+{
+    const r = Math.floor(Math.random()*256);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
 gridContainer.addEventListener('mouseover', function(event)
 {
-    if (event.target.classList.contains('grid-item')) 
-        event.target.style.backgroundColor = 'black';
+    if (event.target.classList.contains('grid-item'))
+    {
+        if(modeDraw==true)
+           event.target.style.backgroundColor = getRandomColor();
+        else 
+           event.target.style.backgroundColor = 'black';
+    }
 });
 
-const button = document.querySelector('button');
+const button = document.querySelector('.sizeButton');
 
 button.onclick = function(){
     let gridSize = prompt("Enter the number of squares per side for the new grid (max 100):");
@@ -30,4 +45,14 @@ button.onclick = function(){
         gridItem.style.height = `calc(100% / ${gridSize})`;
         gridContainer.appendChild(gridItem);
     }
+};
+
+const buttonNoncolors = document.querySelector('#Noncolors');
+buttonNoncolors.onclick = function(){
+    modeDraw=false;
+};
+
+const buttonColors = document.querySelector('#Colors');
+buttonColors.onclick = function(){
+    modeDraw=true;
 };
